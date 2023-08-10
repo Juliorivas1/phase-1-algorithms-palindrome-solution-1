83df4ae98a53194c51fd44326a290ba5378ba86c
Julio Rivas <rivasjulio@ymail.com>
2023-08-10 19:08:06 -0400

    completed

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+
+function reverseString(word){
+
+  const wordArray = word.split("");
+  const reversedWordArray = wordArray.reverse();
+  const reversedWord = reversedWordArray.join("");
+
+  return reversedWord;
+}
+
+
+
+
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  const reversedWord = reverseString(word);
+  if (word === reversedWord) {
+    return true;
+  } else {
+      return false;
+
+    }
+  }
+
 
 /* 
   Add your pseudocode here
@@ -23,3 +44,33 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
+if (require.main === module) {
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+}
